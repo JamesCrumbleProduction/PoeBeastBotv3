b787b89156a0cf3bb1cb5b4f2387e0a0ba8e10f8
atraschinskiy <atraschinskiy@innoseti.ru>
2022-08-18 11:41:41 +0200

    data: remove debug outputs

--- project_updater.py
+++ project_updater.py
@@ -5,24 +5,6 @@ import time
 
 UPDATING_BRANCH: str = 'develop'
 
-# i = 50
-
-# print(f'start restarting -> i: {i}')
-# time.sleep(10)
-# os.execl(sys.executable, sys.executable, *sys.argv)
-
-print(sys.executable, sys.argv)
-repo = git.Repo(os.path.dirname(os.path.abspath(__file__)))
-
-current = repo.head.commit
-
-print(not repo.head.commit.diff('develop'))
-
-if not repo.head.commit.diff('develop'):
-    print("Repo not changed. Sleep mode activated.")
-else:
-    print("Repo changed! Activated.")
-
 
 class ProjectUpdater:
 
@@ -33,18 +15,18 @@ class ProjectUpdater:
             os.path.dirname(os.path.abspath(__file__))
         )
         self._fetch_remotes()
-        self._checkout_to_updating_brach()
+        self._checkout_to_updating_branch()
 
     def _fetch_remotes(self) -> None:
         for remote in self.repo.remotes:
             remote.fetch()
 
-    def _checkout_to_updating_brach(self) -> None:
+    def _checkout_to_updating_branch(self) -> None:
         if self.repo.active_branch.name != UPDATING_BRANCH:
             self.repo.git.checkout(UPDATING_BRANCH)
 
     def _updated(self) -> bool:
-        return not repo.head.commit.diff()
+        return not self.repo.head.commit.diff()
 
     def _update_project(self) -> bool:
         self.repo.git.reset('--hard')
@@ -55,3 +37,6 @@ class ProjectUpdater:
     def pending_update(self) -> None:
         if self._updated():
             return
+
+
+ProjectUpdater().pending_update()
