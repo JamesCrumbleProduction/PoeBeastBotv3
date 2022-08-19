52bef938d848fdfc5feb3c5efda42e0b52eb225f
Artem Traschinsky <68563296+JamesCrumble@users.noreply.github.com>
2022-08-19 11:06:00 +0200

    3.21
    * feat: add more "next_layout_nametags" templates for "beast_bot" action
    
    * feat: add handle ConnectError when trying to init bot data from linking server
    
    * feat: add project autoupdater
    
    * feat: add not complited project updater implemetation
    
    * feat: add updater execution automatically into monitoring_panel and beast_bot
    
    * data: switch updating branch to master
    
    * feat: add enable\disable auto update project and set to false save_settings option for monitoring_panel
    
    * feat: update compile script to auto complete format (#5)
    
    * feat: getting stream of compiled design
    
    * feat: add main algo by replacing but need some fixes
    
    * feat: add full compiling and actully slice_[1] is fine
    
    * fix and feat: fix span adding and complite compile script

--- monitoring_panel/app/__init__.py
+++ monitoring_panel/app/__init__.py
@@ -2,15 +2,17 @@
 import os
 import sys
 
-updater_path: str = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), 
-    *['..','..', 'project_updater'] 
-) 
-sys.path.append(updater_path)
-import updater 
-sys.path.remove(updater_path)
-
 from .settings import settings
+
+if settings.AUTO_UPDATE is True:
+    updater_path: str = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 
+        *['..','..', 'project_updater'] 
+    ) 
+    sys.path.append(updater_path)
+    import updater 
+    sys.path.remove(updater_path)
+
 from .panels import GuiSelector
 from .linking_server import LinkingServer
 
